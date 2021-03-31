a2370d3637bf41eedbe484b71a205dce11984870
Petrs911 <petrs911@gmail.com>
2021-03-31 15:52:51 +0300

    splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  Future<void> startTimer() async {
+    Duration duration = Duration(seconds: 3);
+    return Timer(duration, route);
+  }
+
+  void route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => HomeScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.blueAccent,
+        ),
+      ),
+    );
+  }
+}
